1a51c310a7ee95bed988f71584f8d0ecbdc3ba38
francois xavier paris <fxavierparis@gmail.com>
2021-02-25 17:21:26 +0100

    reste delete a faire

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Dose.destroy_all
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+
+lemon = Ingredient.create!(name: "lemon")
+ice = Ingredient.create!(name: "ice")
+mint = Ingredient.create!(name: "mint leaves")
+
+
+
+misco =Cocktail.create!(name:"Mister Cocktail")
+
+Dose.create!(description:"2cl", cocktail_id: misco.id, ingredient_id: mint.id)
+Dose.create!(description:"2cube", cocktail_id: misco.id, ingredient_id: ice.id)
